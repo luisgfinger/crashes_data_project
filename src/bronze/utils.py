9ec9a4e931b88f107ef3e7869214e54a0367e60f
gulis007 <luisgfinger@gmail.com>
2026-03-04 19:56:26 -0300

    (Delete) Remove unused class

--- src/bronze/utils.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-from datetime import datetime
-
-def parse_start_date_to_where_iso(start_date: str) -> str:
-
-    datetime.strptime(start_date, "%Y-%m-%d")
-    return f"{start_date}T00:00:00.000"
